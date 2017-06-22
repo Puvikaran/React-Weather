6cb28859ff51e56809c6ba060ee10493450d0deb
Puvikaran <puvikaran.s@gmail.com>
2017-06-22 18:55:49 +0530

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 
 var Examples = () =>{
 	return(
-			<h2>Examples Component !</h2>
+			<div>
+				<h2>Examples Component !</h2>
+				<p>Welcome to Examples Page!</p>
+			</div>
 	    );
 
 };
