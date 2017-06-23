b93f155675cca8383403096c007dbbb01e57c5c0
Puvikaran <puvikaran.s@gmail.com>
2017-06-23 11:31:53 +0530

    Added style to Weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,19 @@ var React = require('react');
 
 var About = () => {
 	return(
-			<h2>About Component</h2>
+			<div>
+			<h2 className="text-center">About</h2>
+			<p>React Weather App is built to know the weather in the given location.This is the first react app built to learn react</p>
+			<h5 className="">Tools Used</h5>
+			<dl>
+				<dt>React</dt>
+				<dd>for user interface</dd>
+				<dt>Open Weather Map</dt>
+				<dd>To get the current weather Open Weather Map API is used</dd>
+				<dt>Foundation-Sites</dt>
+				<dd>Foundation-Sites CSS framework is used for styling the App</dd>
+			</dl>
+			</div>
 		);
 };
 
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var WeatherMessage = (props) =>{
 var {temp, location} = props;
 		return(
-			<h2>It's {temp} in {location}</h2>			
+			<h2 classNmae="text-center">It's {temp} in {location}</h2>			
 			);	
 };
 
